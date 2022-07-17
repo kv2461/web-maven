4759f7ef864879683ece92f15929fbc08d8cdc15
kv2461 <kevinarnoldtv@gmail.com>
2022-07-17 00:56:37 -0700

    wrote beginnings of geturl, this should be the function that automatically loads when extension is live

--- server/controllers/ratings.js
+++ server/controllers/ratings.js
@@ -10,6 +10,15 @@ import mongoose from 'mongoose';
 
 //FOLDERS
 export const rateUrl = async (req,res) => {
+
+    //need to first see if the ratings exist, if they don't make a new rating object and add ratings into ratings array
+    //if ratings does exist, check if the object already has the user's ratings
+        //use findIndex/indexOf
+        //if !exist then push to ratings,
+        //if exist then ratings[index] = {} of new ratings
+
+        //call getUrlRatings after
+
     const {url, value} = req.body;
     console.log(url);
     console.log(value);
@@ -19,6 +28,18 @@ export const rateUrl = async (req,res) => {
 
         
     } catch(error) {
+        res.status(404).json({message:error.message});
+    }
+}
+
+
+export const getUrlRatings = async (req,res) => {
+    const {url} = req.body;
+    console.log(url);
+    try {
+        res.status(201).json(req.body);
+
+    } catch (error) {
         res.status(404).json({message:error.message});
     }
 }
